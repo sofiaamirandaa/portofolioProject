e9a54fb8d6296e42baae18a3b225ae86c404750a
Sofia Miranda <sofiabramiranda@gmail.com>
2023-03-16 21:29:23 +0000

    final project

--- js/main.js
+++ js/main.js
@@ -6,12 +6,35 @@ function calculateSeconds () {
 
 
     document.getElementById("seconds").innerHTML = `I've been  programming for ${dif} seconds!`
+}
 
+function getVacation() {
+    const  vacationResult= vacationPlanner()
+    document.getElementById("vacationResult").innerHTML = vacationResult;
+    document.getElementById("vacationButton").innerHTML = "Request another destination"
+
+}
+
+function showPuppyImage() {
+    const puppyElementImage = document.getElementById("puppyYoga");
+    puppyElementImage.hidden = !puppyElementImage.hidden;
+}
+
+function showTravelAgencyImage() {
+    const travelElementImage = document.getElementById("travelAgency");
+    travelElementImage.hidden = !travelElementImage.hidden;
 }
 
 window.onload = function() {
     calculateSeconds();
     setInterval(calculateSeconds, 1000);
- };
- 
- 
+
+    let buttonElement = document.getElementById('vacationButton');
+    buttonElement.addEventListener('click', getVacation);
+    
+    let puppyElement = document.getElementById('appearPuppyYoga');
+    puppyElement.addEventListener('click', showPuppyImage);
+
+    let travelElement = document.getElementById('appearTravelAgency');
+    travelElement.addEventListener('click', showTravelAgencyImage);
+};
